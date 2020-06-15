02a63b6edcfc8fca792fa6eb245d37e4dce7f8fd
Julianto <juliwibowo@outlook.com>
2020-06-15 18:16:48 +0700

    DELETE data REST API pada React.Js

--- src/component/Class/ListComp.jsx
+++ src/component/Class/ListComp.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import qs from 'querystring'
 import { Table, Button, Container, NavLink, Alert } from 'reactstrap'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { type } from 'os'
 
 const api = 'http://localhost:3001'
 
@@ -26,10 +27,43 @@ class ListComp extends Component {
         })
     }
 
+    Deletemahasiswa = (id_mahasiswa) => {
+        const { mahasiswa } = this.state
+        const data = qs.stringify({
+            id_mahasiswa: id_mahasiswa
+
+        })
+
+        axios.delete(api + '/hapus',
+            {
+                data: data,
+                headers: { 'Content-type': 'application/x-www-form-urlencoded' }
+            }
+        ).then(json => {
+            if (json.data.status === 200) {
+                this.setState({
+                    response: json.data.values,
+                    mahasiswa: mahasiswa.filter(mahasiswa => mahasiswa.id_mahasiswa !== id_mahasiswa),
+                    display: 'block'
+                })
+                this.props.history.push('/mahasiswa')
+            } else {
+                this.setState({
+                    response: json.data.values,
+                    display: 'block'
+                })
+            }
+        })
+    }
+
+
     render() {
         return (
             <Container>
                 <h2> Data Mahasiswa</h2>
+                <Alert color="success" style={{ display: this.state.display }}>
+                    {this.state.response}
+                </Alert>
                 <NavLink href="/mahasiswa/tambah"><Button color="success">Tambah Data</Button></NavLink>
                 <hr />
                 <Table classname="table-bordered">
@@ -49,19 +83,22 @@ class ListComp extends Component {
                                 <td>{mahasiswa.jurusan}</td>
                                 <td>
                                     <Link to=
-                                    {
                                         {
-                                            pathname: '/mahasiswa/edit',
-                                            state: {
-                                                id_mahasiswa: mahasiswa.id_mahasiswa,
-                                                nim: mahasiswa.nim,
-                                                nama: mahasiswa.nama,
-                                                jurusan: mahasiswa.jurusan
+                                            {
+                                                pathname: '/mahasiswa/edit',
+                                                state: {
+                                                    id_mahasiswa: mahasiswa.id_mahasiswa,
+                                                    nim: mahasiswa.nim,
+                                                    nama: mahasiswa.nama,
+                                                    jurusan: mahasiswa.jurusan
+                                                }
                                             }
-                                        }
-                                    }>
+                                        }>
                                         <Button> Edit </Button>
+                                       
                                     </Link>
+                                    <span> </span>
+                                        <Button onClick={() => this.Deletemahasiswa(mahasiswa.id_mahasiswa)} color="danger"> Hapus </Button>
                                 </td>
                             </tr>
                         )}
